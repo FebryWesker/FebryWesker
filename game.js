900ea1f2ad3d585148e84eff0c062291d5b58908
Febry Wesker (Azbry-MD) <sybry1148@gmail.com>
2025-10-18 16:22:43 +0800

    Delete game.js

--- game.js
+++
@@ -1,9 +0,0 @@
-- const birdImg = new Image();
-- birdImg.src = "https://files.catbox.moe/9whqg7.jpg";
-+ const birdImg = new Image();
-+ birdImg.src = "./assets/img/bird.png";
-
-- const bgImg = new Image();
-- bgImg.src = "https://files.catbox.moe/0ub3wk.jpg";
-+ const bgImg = new Image();
-+ bgImg.src = "./assets/img/bg-city.png";
